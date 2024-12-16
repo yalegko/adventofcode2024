c8508b4549ebab923f35332cf6632a6f5a353c5d
Oleg Broslavskiy <yalegko@gmail.com>
2024-12-16 16:05:21 +0700

    Try to cleanup day16 a bit

--- day16.ml
+++ day16.ml
@@ -11,48 +11,64 @@ let turn_cw d =
 let turn_ccw d = d |> turn_cw |> turn_cw |> turn_cw
 let () = assert (Char.equal (turn_ccw '>') '^')
 
-let best_way field ~from ~to' =
+type state = { score : int; direction : char; path : (int * int) list }
+
+let do_step state =
+  let pos = List.hd_exn state.path in
+  let next = Myfield.do_step ~direction:state.direction pos in
+  { state with path = next :: state.path; score = state.score + 1 }
+
+let turn state turn =
   let turn_cost = 1000 in
-  let visited = Hashtbl.create (module Myfield.PointWithDir) in
+  {
+    state with
+    direction = turn state.direction;
+    score = state.score + turn_cost;
+  }
+
+let best_way field ~from =
   let best_paths = ref [] in
-  let best_score =
-    ref (turn_cost * Array.length field * Array.length field.(0))
-  in
+  let best_score = ref 100500 in
+  let visited = Hashtbl.create (module Myfield.PointWithDir) in
   let rec bfs queue =
     match queue with
     | [] -> (!best_score, !best_paths)
-    (* Finish *)
-    | ((pos :: _ as path), _direction, score) :: tail
-      when Myfield.Point.equal pos to' ->
-        if score < !best_score then (
-          best_score := score;
-          best_paths := [ path ])
-        else if score = !best_score then best_paths := path :: !best_paths;
-        bfs tail
-    (* Wall *)
-    | ((x, y) :: _, _, _) :: tail when Char.equal field.(x).(y) '#' -> bfs tail
-    (* Keep going *)
-    | ((pos :: _ as path), direction, score) :: tail -> (
-        match Hashtbl.find visited (pos, direction) with
-        | Some old_score when old_score < score -> bfs tail
-        | _ ->
-            Hashtbl.set visited ~key:(pos, direction) ~data:score;
-            bfs
-              (List.append tail
-                 [
-                   (Myfield.do_step ~direction pos :: path, direction, score + 1);
-                   (path, turn_cw direction, score + turn_cost);
-                   (path, turn_ccw direction, score + turn_cost);
-                 ]))
-    | ([], _, _) :: _tail -> failwith "unreachable"
+    | state :: tail -> (
+        let ((x, y) as pos) = List.hd_exn state.path in
+        match field.(x).(y) with
+        | '#' ->
+            (* Wall *)
+            bfs tail
+        | 'E' when state.score < !best_score ->
+            (* New best *)
+            best_score := state.score;
+            best_paths := [ state.path ];
+            bfs tail
+        | 'E' when state.score = !best_score ->
+            (* Current best *)
+            best_paths := state.path :: !best_paths;
+            bfs tail
+        | _ when state.score >= !best_score ->
+            (* Went too deep *)
+            bfs tail
+        | _ -> (
+            (* Keep going *)
+            match Hashtbl.find visited (pos, state.direction) with
+            | Some old_score when old_score < state.score -> bfs tail
+            | _ ->
+                Hashtbl.set visited ~key:(pos, state.direction)
+                  ~data:state.score;
+                bfs
+                  (List.append tail
+                     [ do_step state; turn state turn_cw; turn state turn_ccw ])
+            ))
   in
-  bfs [ ([ from ], '>', 0) ]
+  bfs [ { score = 0; path = [ from ]; direction = '>' } ]
 
 let solve1 fname =
   let field = Myfield.read fname in
   let start = Myfield.find field ~f:(Char.equal 'S') |> List.hd_exn in
-  let finish = Myfield.find field ~f:(Char.equal 'E') |> List.hd_exn in
-  best_way field ~from:start ~to':finish |> fst
+  best_way field ~from:start |> fst
 
 let () =
   (* assert (Util.time solve1 "data/day16.txt" = 83432); *)
@@ -62,9 +78,8 @@ let () =
 let solve2 fname =
   let field = Myfield.read fname in
   let start = Myfield.find field ~f:(Char.equal 'S') |> List.hd_exn in
-  let finish = Myfield.find field ~f:(Char.equal 'E') |> List.hd_exn in
 
-  let best_paths = best_way field ~from:start ~to':finish |> snd in
+  let best_paths = best_way field ~from:start |> snd in
   List.concat best_paths
   |> List.dedup_and_sort ~compare:Myfield.Point.compare
   |> List.length
