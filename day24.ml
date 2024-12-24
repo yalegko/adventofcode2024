b5fdbd6aee6dafec3bf33856b719640e0e92f8ea
Oleg Broslavskiy <yalegko@gmail.com>
2024-12-24 22:08:20 +0700

    Cleanup day24 a bit

--- day24.ml
+++ day24.ml
@@ -1,7 +1,6 @@
 open Core
 
 type gate = Const of int | Logical of (string * string * string)
-[@@deriving sexp, compare]
 
 let cut s ~sep =
   Re.split (Re.compile (Re.str sep)) s |> function
@@ -59,86 +58,112 @@ let () =
   assert (solve1 "test/day24.txt" = 4);
   assert (solve1 "data/day24.txt" = 46463754151024)
 
-let gate_key (g1, op, g2) =
-  List.sort ~compare:String.compare [ g1; op; g2 ] |> String.concat ~sep:" "
+let normalize_logic (g1, op, g2) =
+  let g1, g2 = if String.compare g1 g2 < 0 then (g1, g2) else (g2, g1) in
+  (g1, op, g2)
 
-let rec expect ~scheme ~gates z ((a, op, b) as expgate) =
-  let get_logical gate =
-    match Hashtbl.find_exn scheme gate with
+let gate_key l =
+  let g1, op, g2 = normalize_logic l in
+  g1 ^ " " ^ op ^ " " ^ g2
+
+let rec expect ~scheme ~gates name expected_logic =
+  let get_logical gate_name =
+    match Hashtbl.find_exn scheme gate_name with
     | Logical (g1, op, g2) -> (g1, op, g2)
     | _ -> failwith "unreachable"
   in
 
-  let ((zl, zop, zr) as zgate) = get_logical z in
+  let exp1, eop, exp2 = expected_logic |> normalize_logic in
+  let ((gate1, op, gate2) as gate_logic) =
+    get_logical name |> normalize_logic
+  in
 
-  if String.equal (gate_key zgate) (gate_key expgate) then None
+  if String.equal (gate_key gate_logic) (gate_key expected_logic) then
+    (* Logic connected correctly -- proceed *)
+    None
   else
-    match Hashtbl.find gates (gate_key expgate) with
-    | Some actual ->
-        printf "Swap %s -> %s!\n" z actual;
-        printf "\t %s = %s %s %s\n" z zl zop zr;
-        printf "\t %s = %s %s %s\n" actual a op b;
-
-        Hashtbl.set gates ~key:(gate_key zgate) ~data:actual;
-        Hashtbl.set gates ~key:(gate_key expgate) ~data:z;
-        Hashtbl.set scheme ~key:z ~data:(Logical expgate);
-        Hashtbl.set scheme ~key:actual ~data:(Logical zgate);
-
-        Some (z, actual)
+    match Hashtbl.find gates (gate_key expected_logic) with
+    (* Already have expected logic under different key -- found a swap *)
+    | Some actual_name ->
+        Hashtbl.set gates ~key:(gate_key gate_logic) ~data:actual_name;
+        Hashtbl.set gates ~key:(gate_key expected_logic) ~data:name;
+        Hashtbl.set scheme ~key:name ~data:(Logical expected_logic);
+        Hashtbl.set scheme ~key:actual_name ~data:(Logical gate_logic);
+        Some (name, actual_name)
+    (* Can't find expected logic in scheme, look in operands *)
     | None ->
-        if String.equal zl a then expect ~scheme ~gates zr (get_logical b)
-        else if String.equal zl b then expect ~scheme ~gates zr (get_logical a)
-        else if String.equal zr a then expect ~scheme ~gates zl (get_logical b)
-        else if String.equal zr b then expect ~scheme ~gates zl (get_logical a)
+        assert (String.equal eop op);
+        if String.equal gate1 exp1 then
+          expect ~scheme ~gates gate2 (get_logical exp2)
+        else if String.equal gate2 exp2 then
+          expect ~scheme ~gates gate1 (get_logical exp1)
         else failwith "unreachable"
 
-let _ =
-  let scheme = read_scheme "data/day24.txt" in
+let solve2 fname =
+  (* Scheme maps gate name to logic *)
+  let scheme =
+    read_scheme fname
+    |> Hashtbl.map ~f:(function
+         | Logical l -> Logical (normalize_logic l)
+         | const -> const)
+  in
 
-  let gates = String.Table.create () in
-
-  Hashtbl.to_alist scheme
-  |> List.iter ~f:(fun (name, wire) ->
-         match wire with
-         | Const _ -> ()
-         | Logical l -> Hashtbl.add_exn gates ~key:(gate_key l) ~data:name);
-
+  (* Gates maps gate logic to name *)
+  let gates =
+    Hashtbl.to_alist scheme
+    |> List.filter_map ~f:(fun (name, wire) ->
+           match wire with
+           | Const _ -> None
+           | Logical l -> Some (gate_key l, name))
+    |> String.Table.of_alist_exn
+  in
   let name_of logical =
     match Hashtbl.find gates (gate_key logical) with
     | Some v -> v
     | None -> failwithf "can't find %s" (gate_key logical) ()
   in
 
+  (* Too tired to collect result functionally *)
   let res = ref [] in
+  let mb_add_to_res some_swap =
+    match some_swap with Some (s1, s2) -> res := s1 :: s2 :: !res | _ -> ()
+  in
+
+  (* Here we implement a summator logic like x + y = z:
+
+         z0 = x0 ^ y0
+         carry = x0 & y1
+
+         zi = xi ^ yi ^ carry
+         carry = xi & yi | (xi ^ yi) & carry
+
+         zn = carry
+
+     Ref: https://en.wikipedia.org/wiki/Adder_(electronics)#Full_adder
+  *)
+  expect ~scheme ~gates "z00" ("x00", "XOR", "y00") |> mb_add_to_res;
   let carry =
-    List.range 0 45
-    |> List.fold_left ~init:None ~f:(fun carry i ->
+    List.range 1 45
+    |> List.map ~f:(fun i ->
            let x = sprintf "x%02d" i
            and y = sprintf "y%02d" i
            and z = sprintf "z%02d" i in
+           (x, y, z))
+    |> List.fold_left ~init:("x00", "AND", "y00") ~f:(fun carry (xi, yi, zi) ->
+           (* zi = xi ^ yi ^ carry *)
+           expect ~scheme ~gates zi
+             (name_of (xi, "XOR", yi), "XOR", name_of carry)
+           |> mb_add_to_res;
 
-           match carry with
-           | None ->
-               let some_swap = expect ~scheme ~gates z (x, "XOR", y) in
-               (match some_swap with
-               | Some (s1, s2) -> res := s1 :: s2 :: !res
-               | _ -> ());
-               Some (x, "AND", y)
-           | Some carry ->
-               (* zi = xi ^ yi ^ carry *)
-               let some_swap =
-                 expect ~scheme ~gates z
-                   (name_of (x, "XOR", y), "XOR", name_of carry)
-               in
-               (match some_swap with
-               | Some (s1, s2) -> res := s1 :: s2 :: !res
-               | _ -> ());
-
-               (* carry = (xi & yi) | (xi ^ yi) & carry *)
-               Some
-                 ( name_of (x, "AND", y),
-                   "OR",
-                   name_of (name_of (x, "XOR", y), "AND", name_of carry) ))
+           (* carry = (xi & yi) | (xi ^ yi) & carry *)
+           ( name_of (xi, "AND", yi),
+             "OR",
+             name_of (name_of (xi, "XOR", yi), "AND", name_of carry) ))
   in
+  expect ~scheme ~gates "z45" carry |> mb_add_to_res;
 
   List.sort !res ~compare:String.compare |> String.concat ~sep:","
+
+let () =
+  assert (
+    String.equal (solve2 "data/day24.txt") "cqk,fph,gds,jrs,wrk,z15,z21,z34")
